55e7e3bc6e7eb98e1840fab263a93ec8e304fe81
Jordibz23 <0333191025@unjfsc.edu.pe>
2023-08-16 20:06:13 -0500

    cambinado a api incidencias

--- src/controllers/datos.controller.js
+++ src/controllers/datos.controller.js
@@ -1,28 +1,31 @@
 import { pool } from '../db.js'
 
-export const tb_matricula = async (req, res) => {
-    const [result] = await pool.query('SELECT m.* ,e.nombre as nombreEn from matricula m inner join encargado e on m.id_encargado = e.id_encargado;')
+export const tb_area = async (req, res) => {
+    const [result] = await pool.query('SELECT * FROM  tb_area;')
     res.send(result)
 }
 
 export const tb_encargado = async (req, res) => {
-    const [result] = await pool.query('Select id_encargado, nombre from encargado')
+    const [result] = await pool.query('Select * from tb_encargado')
     res.send(result)
 }
 
-export const ultimoID = async (req, res) => {
-    const [result] = await pool.query('SELECT id_matricula from matricula order by id_matricula DESC LIMIT 1;')
-    res.send(result[0])
+export const tb_tipo_incidencias = async (req, res) => {
+    const [result] = await pool.query('SELECT * FROM tb_tipo_incidencia;')
+    res.send(result)
 }
 
-export const insert_matricula = async (req, res) => {
+export const cantidad_incidencia = async (req, res) => {
+    const [result] = await pool.query('SELECT count(*) as cantidad, ti.nom_tipo_incidencia FROM `tb_incidencias` d inner join tb_tipo_incidencia ti on ti.id_tipo_incidencia= d.id_tipo_incidencia GROUP BY d.id_tipo_incidencia;')
+    res.send(result)
+}
+
+export const insert_incidencia = async (req, res) => {
     try {
-        const { id_matricula, apoderado, fecha, grado, monto, nivel, tiempo, tipo_pago, tipo_postulante,
-            id_encargado } = req.body
-        const [rows] = await pool.query('INSERT INTO matricula values(?,?,?,?,?,?,?,?,?,?)',
-            [id_matricula, apoderado, fecha, grado, monto, nivel, tiempo, tipo_pago, tipo_postulante,
-                id_encargado])
-        res.send({ message: 'Matricula guardada correctamente' })
+        const { id_usuario, id_area, id_tipo_incidencia, descripcion_incidencia, fecha_incidencia, estado_incidencia } = req.body
+        const [rows] = await pool.query('INSERT INTO matricula( id_usuario, id_area, id_tipo_incidencia, descripcion_incidencia, fecha_incidencia, estado_incidencia) values(?,?,?,?,?,?)',
+            [id_usuario, id_area, id_tipo_incidencia, descripcion_incidencia, fecha_incidencia, estado_incidencia])
+        res.send({ message: 'Incidencia guardada correctamente' })
     } catch (error) {
         return res.send({ message: error })
     }
--- src/routes/datos.router.js
+++ src/routes/datos.router.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
-import { tb_encargado, tb_matricula, ultimoID, insert_matricula } from '../controllers/datos.controller.js'
+import { tb_encargado, tb_area, tb_tipo_incidencias, insert_incidencia, cantidad_incidencia } from '../controllers/datos.controller.js'
 
 const router = Router()
 
 router.get('/datos/encargado', tb_encargado)
-router.get('/datos/matricula', tb_matricula)
-router.get('/datos/ultimoid', ultimoID)
-router.post('/datos/nuevaMatricula', insert_matricula)
+router.get('/datos/areas', tb_area)
+router.get('/datos/tipo-incidencias', tb_tipo_incidencias)
+router.post('/datos/nueva-incidencia', insert_incidencia)
+router.get('/datos/cantidad-incidencia', cantidad_incidencia)
 router.post('/datos/enviar', (req, res) => {
     console.log(req.body)
     console.log(req.files);
