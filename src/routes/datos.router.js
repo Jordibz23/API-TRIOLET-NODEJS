9ce232bb7bef537c3c7f7a3b6601fc239a999355
Jordibz23 <0333191025@unjfsc.edu.pe>
2023-08-17 09:20:18 -0500

    finalizado

--- src/routes/datos.router.js
+++ src/routes/datos.router.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { tb_encargado, tb_area, tb_tipo_incidencias, insert_incidencia, cantidad_incidencia_agrupado, cantidad_incidencias, incidencia_mas_frecuente, area_mas_incidencia, listado_tabla, actualizar_incidencia, incidencias_resueltas, promedio_resolucion } from '../controllers/datos.controller.js'
-
+import { buscar_tareas, resolver_incidencia } from '../controllers/encargado.controller.js'
 const router = Router()
 
 router.get('/datos/encargado', tb_encargado)
@@ -15,6 +15,10 @@ router.get('/datos/incidencias-resueltas', incidencias_resueltas)
 router.get('/datos/incidencia-mas-frecuente', incidencia_mas_frecuente)
 router.get('/datos/area-mas-incidencia', area_mas_incidencia)
 router.get('/datos/listado-tabla', listado_tabla)
+router.post('/encargado/buscar', buscar_tareas)
+router.post('/encargado/resolver', resolver_incidencia)
+
+
 
 router.post('/datos/enviar', (req, res) => {
     console.log(req.body)
