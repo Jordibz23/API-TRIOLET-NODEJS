60a320763c9f08a802039f617fb4106b7ea9616d
Jordibz23 <0333191025@unjfsc.edu.pe>
2023-08-17 05:35:49 -0500

    actualizaciones

--- src/controllers/datos.controller.js
+++ src/controllers/datos.controller.js
@@ -15,8 +15,37 @@ export const tb_tipo_incidencias = async (req, res) => {
     res.send(result)
 }
 
-export const cantidad_incidencia = async (req, res) => {
+export const cantidad_incidencias = async (req, res) => {
+    const [result] = await pool.query('SELECT count(*) as cantidad FROM `tb_incidencias` ;')
+    res.send(result)
+}
+
+export const cantidad_incidencia_agrupado = async (req, res) => {
     const [result] = await pool.query('SELECT count(*) as cantidad, ti.nom_tipo_incidencia FROM `tb_incidencias` d inner join tb_tipo_incidencia ti on ti.id_tipo_incidencia= d.id_tipo_incidencia GROUP BY d.id_tipo_incidencia;')
+    res.send(result)
+}
+export const incidencias_resueltas = async (req, res) => {
+    const [result] = await pool.query('SELECT count(*) as cantidad, ti.nom_tipo_incidencia, ROUND(AVG(TIME_TO_SEC(d.tiempo_resolucion) / 60)) as promedio FROM tb_incidencias d INNER JOIN   tb_tipo_incidencia ti ON ti.id_tipo_incidencia = d.id_tipo_incidencia WHERE d.estado_incidencia = 2 GROUP BY d.id_tipo_incidencia, ti.nom_tipo_incidencia;')
+    res.send(result)
+}
+export const promedio_resolucion = async (req, res) => {
+    const [result] = await pool.query('SELECT ROUND(AVG(TIME_TO_SEC(tiempo_resolucion) / 60)) as cantidad FROM tb_incidencias;')
+    res.send(result)
+}
+
+
+export const incidencia_mas_frecuente = async (req, res) => {
+    const [result] = await pool.query('SELECT count(*) as otro, ti.nom_tipo_incidencia as cantidad FROM `tb_incidencias` d inner join tb_tipo_incidencia ti on ti.id_tipo_incidencia= d.id_tipo_incidencia GROUP BY d.id_tipo_incidencia limit 1;')
+    res.send(result)
+}
+
+export const area_mas_incidencia = async (req, res) => {
+    const [result] = await pool.query('SELECT count(*) as otro, a.nom_area as cantidad FROM `tb_incidencias` d inner join tb_area a on a.id_area= d.id_area GROUP BY d.id_area limit 1;')
+    res.send(result)
+}
+
+export const listado_tabla = async (reg, res) => {
+    const [result] = await pool.query("SELECT t.id_incidencias as id , a.nom_area as area, ti.nom_tipo_incidencia as t_inci,DATE_FORMAT(t.fecha_incidencia, '%Y-%m-%d') as f_inci, t.estado_incidencia as e_inci , t.id_encargado as enc, DATE_FORMAT(t.fecha_solucion, '%Y-%m-%d') as f_sol, t.tiempo_resolucion as t_sol , t.descripcion_incidencia as detalle, u.nom_usuario as usua FROM `tb_incidencias` t inner join tb_usuarios u on u.id_usuario = t.id_usuario inner join tb_tipo_incidencia ti on ti.id_tipo_incidencia = t.id_tipo_incidencia inner join tb_area a on a.id_area = t.id_area;")
     res.send(result)
 }
 
@@ -29,6 +58,15 @@ export const insert_incidencia = async (req, res) => {
     } catch (error) {
         return res.send({ message: 'Ocurrio un error al guardar el incidente, verifique los campos' })
     }
+}
 
-
+export const actualizar_incidencia = async (req, res) => {
+    try {
+        const { id_incidencias, estado_incidencia, fecha_designacion_encargado, id_encargado } = req.body
+        const [rows] = await pool.query('UPDATE tb_incidencias SET estado_incidencia = ? , fecha_designacion_encargado = ?, id_encargado = ? WHERE id_incidencias = ?;',
+            [estado_incidencia, fecha_designacion_encargado, id_encargado, id_incidencias])
+        res.send({ message: 'Incidencia guardada correctamente' })
+    } catch (error) {
+        return res.send({ message: 'Ocurrio un error al actualizar el incidente, verifique los campos' })
+    }
 }
--- src/routes/datos.router.js
+++ src/routes/datos.router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { tb_encargado, tb_area, tb_tipo_incidencias, insert_incidencia, cantidad_incidencia } from '../controllers/datos.controller.js'
+import { tb_encargado, tb_area, tb_tipo_incidencias, insert_incidencia, cantidad_incidencia_agrupado, cantidad_incidencias, incidencia_mas_frecuente, area_mas_incidencia, listado_tabla, actualizar_incidencia, incidencias_resueltas, promedio_resolucion } from '../controllers/datos.controller.js'
 
 const router = Router()
 
@@ -7,7 +7,15 @@ router.get('/datos/encargado', tb_encargado)
 router.get('/datos/areas', tb_area)
 router.get('/datos/tipo-incidencias', tb_tipo_incidencias)
 router.post('/datos/nueva-incidencia', insert_incidencia)
-router.get('/datos/cantidad-incidencia', cantidad_incidencia)
+router.post('/datos/update-incidencia', actualizar_incidencia)
+router.get('/datos/cantidad-incidencia-agrupado', cantidad_incidencia_agrupado)
+router.get('/datos/cantidad-incidencias', cantidad_incidencias)
+router.get('/datos/promedio-resolucion', promedio_resolucion)
+router.get('/datos/incidencias-resueltas', incidencias_resueltas)
+router.get('/datos/incidencia-mas-frecuente', incidencia_mas_frecuente)
+router.get('/datos/area-mas-incidencia', area_mas_incidencia)
+router.get('/datos/listado-tabla', listado_tabla)
+
 router.post('/datos/enviar', (req, res) => {
     console.log(req.body)
     console.log(req.files);
